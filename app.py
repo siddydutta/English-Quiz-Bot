ba42138e3bce7477e3593cbd2b909a0daa4e56ee
siddydutta <siddharthadutta@protonmail.com>
2022-12-10 15:58:58 +0530

    Feature: Stop Quiz

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-import bson
+from datetime import datetime
 
 from flask import Flask, request
 from flask_swagger_ui import get_swaggerui_blueprint
-from datetime import datetime
-from quiz_db import get_quiz_questions, add_poll, get_poll, add_answer, add_quiz_question
-from bot import send_poll
+
+from bot import send_poll, stop_poll
+from quiz_db import get_quiz_questions, add_poll, get_poll, add_answer, add_quiz_question, get_active_polls, \
+    update_poll_status
 
 app = Flask(__name__)
+
 # START Swagger Specification #
 SWAGGER_URL = '/swagger'
 API_URL = '/static/swagger.json'
@@ -14,7 +16,7 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
     config={
-        'app_name': "English-Quiz-Bot"
+        'app_name': 'English-Quiz-Bot'
     }
 )
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
@@ -36,15 +38,31 @@ def add_question():
     return 'ok', 200
 
 
-@app.route('/send-quiz', methods=['GET'])
-def send_quiz():
-    quiz_no = int(request.args.get("quiz_no"))
+@app.route('/start-quiz', methods=['GET'])
+def start_quiz():
+    quiz_no = int(request.args.get('quiz_no'))
     questions = get_quiz_questions(quiz_no)
     for question in questions:
-        poll_id = send_poll(question.get('question'), question.get('options'), question.get('correct_option_id'),
-                            question.get('explanation'))
-        add_poll(poll_id, question['correct_option_id'], quiz_no)
+        result = send_poll(question.get('question'), question.get('options'), question.get('correct_option_id'),
+                           question.get('explanation'))
+        poll = {'poll_id': result.get('poll').get('id'),
+                'message_id': result.get('message_id'),
+                'correct_option_id': question.get('correct_option_id'),
+                'quiz_no': quiz_no,
+                'posted': datetime.utcnow(),
+                'active': True}
+        add_poll(poll)
     return 'ok', 200
+
+
+@app.route('/stop-quiz', methods=['PUT'])
+def stop_quiz():
+    quiz_no = int(request.args.get('quiz_no'))
+    polls = get_active_polls(quiz_no)
+    for poll in polls:
+        stop_poll(poll.get('message_id'))
+    update_poll_status(quiz_no)
+    return '', 200
 
 
 @app.route('/webhook-poll-answer', methods=['POST'])
@@ -52,7 +70,7 @@ def process_poll_answer_update():
     update = request.json
     if 'poll_answer' not in update:
         # Don't process other updates
-        return "", 204
+        return '', 204
 
     poll_answer = update['poll_answer']
     poll = get_poll(poll_answer.get('poll_id'))
@@ -67,7 +85,7 @@ def process_poll_answer_update():
               'score': score,
               'answered': datetime.utcnow()}
     add_answer(answer)
-    return "", 200
+    return '', 200
 
 
 if __name__ == '__main__':
--- quiz_db.py
+++ quiz_db.py
@@ -10,7 +10,7 @@ def get_db():
     """
     Configuration method to return db instance
     """
-    db = getattr(g, "_database", None)
+    db = getattr(g, '_database', None)
     if db is None:
         db = g._database = PyMongo(current_app, os.environ.get('MONGO_QUIZ_URI')).db
     return db
@@ -28,13 +28,21 @@ def add_quiz_question(question):
     db.questions.insert_one(question)
 
 
-def add_poll(poll_id, correct_option_id, quiz_no):
-    poll = {'poll_id': poll_id, 'correct_option_id': correct_option_id, 'quiz_no': quiz_no, 'posted': datetime.utcnow()}
+def add_poll(poll):
     db.polls.insert_one(poll)
 
 
 def get_poll(poll_id):
     return db.polls.find_one({'poll_id': poll_id})
+
+
+def get_active_polls(quiz_no: int):
+    return [poll for poll in db.polls.find({'quiz_no': quiz_no, 'active': True})]
+
+
+def update_poll_status(quiz_no: int):
+    db.polls.update_many({'quiz_no': quiz_no, 'active': True}, {'$set': {'stopped': datetime.utcnow(),
+                                                                         'active': False}}, upsert=False)
 
 
 def add_answer(answer):
